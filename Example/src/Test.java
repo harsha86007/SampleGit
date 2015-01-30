625885612f95aa68be40268daebbf276dcb53207
harsha talluri <harsha86007@gmail.com>
2015-01-29 22:46:57 -0400

    Initial git

---
+++ Example/src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args)
+    {
+       int a=70,b=50;
+       String s;
+       s= (a>b) ? "A is big" : "B is big";
+       System.out.println((a==b)? "A and B are same" : "" +s);
+    }
+}
